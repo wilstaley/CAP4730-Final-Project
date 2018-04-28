f4588c6cca5c6fadf1de8047f3cd97e71ab0b35c
Wilson Staley <wilstaley@gmail.com>
2018-04-28 16:48:23 -0400

    Added Box and Plant Meshes

--- js/main.js
+++ js/main.js
@@ -1,124 +1,109 @@
-//setup
+//Setup
 let width = window.innerWidth;
 let height = window.innerHeight;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, width/height, 1, 1000 );
                                               //fov,aspect,near,far
-
+camera.position.z = 5;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
-
-
-
-//Add light to the scene
-var light = new THREE.AmbientLight( 0xf0f0f0 ); // soft white light
-scene.add( light );
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-directionalLight.position = (0,0,1);
-scene.add( directionalLight );
 
 
 //Add torus to the scene
 var geometry = new THREE.TorusKnotBufferGeometry( 10, 3, 200, 16, 20, 3);
 var material = new THREE.MeshNormalMaterial();
 var knot = new THREE.Mesh( geometry, material );
+knot.castShadow = true;
 scene.add( knot );
-
 knot.position.z = -50;
-camera.position.z = 5;
-
-var text1 = new THREE.Mesh();
-var text2 = new THREE.Mesh();
-
-//Add text to the scene
-var loader = new THREE.FontLoader();
-
-loader.load( 'font.json', function ( font ) {
-
-	var U = new THREE.TextGeometry( 'U', {
-		font: font,
-		size: 8,
-		height: 20,
-		curveSegments: 2,
-	} );
-
-	var F = new THREE.TextGeometry( 'F', {
-		font: font,
-		size: 8,
-		height: 20,
-		curveSegments: 2,
-	} );
-
-	var textMaterial1 = new THREE.MeshLambertMaterial({
-		wireframe: false,
-		color: 0xff963a
-	});
-	textMaterial1.needsUpdate = true;
-	var textMaterial2 = new THREE.MeshLambertMaterial({
-		wireframe: false,
-		color: 0x3657f9
-	});
-
-	text1 = new THREE.Mesh( U, textMaterial1 );
-	text1.visible = false;
-	scene.add(text1);
-	text2 = new THREE.Mesh( F, textMaterial2 );
-	text2.visible = false;
-	scene.add(text2);
-	// var pos = new THREE.Vector3(0,0,0);
-	// text.position = pos;
-	text1.position.z = -50;
-	text1.position.x = -4;
-	text1.position.y = -2;
-	text2.position.z = -50;
-	text2.position.x = 1;
-	text2.position.y = -2;
-});
-
-// // instantiate a loader
-// var loader = new THREE.OBJLoader();
-//
-// // load a resource
-// loader.load(
-// 	// resource URL
-// 	'plant.obj',
-// 	// called when resource is loaded
-// 	function ( object ) {
-//
-// 		scene.add( object );
-//
-// 	},
-// 	// called when loading is in progresses
-// 	function ( xhr ) {
-//
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-//
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-//
-// 		console.log( 'An error happened' );
-//
-// 	}
-// );
-//
-// // Alternatively, to parse a previously loaded JSON structure
-// var object = loader.parse( a_json_object );
-//
-// scene.add( object );
 
 
+//Load texture for cube
+var texture = new THREE.TextureLoader().load( "textures/metalbox_full.png" );
+
+//Add cube to the scene
+var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+var material = new THREE.MeshLambertMaterial( {
+	map: texture
+} );
+// material.lights = true;
+var cube = new THREE.Mesh( geometry, material );
+cube.position.z = -50;
+cube.visible = false;
+scene.add( cube );
+
+
+var loader = new THREE.TGALoader();
+var plant_texture = loader.load(
+	'textures/plant_texture.tga',
+	//called when loading is completed
+	function ( texture ) {
+		console.log( 'Texture is loaded' );
+	},
+	//called when the loading is in progresses
+	function ( xhr ) {
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+	//called when the loading fails
+	function ( error ) {
+		console.log( 'An error happened' );
+	}
+);
+
+var id;
+function showPlant(display){
+	var objLoader = new THREE.OBJLoader();
+	objLoader.load('plant.obj',
+		//called when loading is completed
+		function (obj) {
+			if(display){
+		    obj.traverse(function (child) {
+		        if (child instanceof THREE.Mesh) {
+		            child.material = new THREE.MeshLambertMaterial(
+									{map: plant_texture});
+		        }
+		    });
+				obj.position.z = -50;
+				obj.position.y = -20;
+				id = obj.id;
+				var animate = function () {
+					requestAnimationFrame( animate );
+				  obj.rotation.y += .01;
+					renderer.render(scene, camera);
+				};
+				animate();
+		    scene.add(obj);
+			}
+			else{
+		    var object = scene.getObjectById(id, true);
+				scene.remove(object);
+			}
+		},
+		//called when loading is in progresses
+		function ( xhr ) {
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		},
+		//called when loading has errors
+		function ( error ) {
+			console.log( 'An error happened' );
+		}
+	);
+}
+
+
+//Animate the meshes
 var animate = function () {
 	requestAnimationFrame( animate );
   knot.rotation.x += .03;
   knot.rotation.y += .01;
+	// cube.rotation.x += .01;
+	cube.rotation.y += .01;
 	renderer.render(scene, camera);
 };
-
 animate();
+
 
 //Adjust aspect ratio for window resize
 window.addEventListener('resize', onWindowResize, false);
@@ -129,12 +114,10 @@ function onWindowResize(){
 }
 
 
-
+//Update camera's projection matrix on FOV slider change
 var fovSlider = document.getElementById("fovRange");
 var fovOutput = document.getElementById("fovNum");
 fovOutput.innerHTML = fovSlider.value; // Display the default slider value
-
-//On slider change
 fovSlider.oninput = function() {
     //change slider's display value
     fovOutput.innerHTML = this.value;
@@ -144,12 +127,10 @@ fovSlider.oninput = function() {
 }
 
 
-
+//Update camera's projection matrix on Near slider change
 var nearSlider = document.getElementById("nearRange");
 var nearOutput = document.getElementById("nearNum");
 nearOutput.innerHTML = nearSlider.value; // Display the default slider value
-
-//On slider change
 nearSlider.oninput = function() {
     //change slider's display value
     nearOutput.innerHTML = this.value;
@@ -159,12 +140,10 @@ nearSlider.oninput = function() {
 }
 
 
-
+//Update camera's projection matrix on Far slider change
 var farSlider = document.getElementById("farRange");
 var farOutput = document.getElementById("farNum");
 farOutput.innerHTML = farSlider.value; // Display the default slider value
-
-//On slider change
 farSlider.oninput = function() {
     //change slider's display value
     farOutput.innerHTML = this.value;
@@ -179,15 +158,21 @@ var select = document.getElementById("model-select");
 select.addEventListener("change", ()=>{
 	var selectedModel = select.options[select.selectedIndex].value;
 	if(selectedModel === "torus"){
-		text1.visible = false;
-		text2.visible = false;
+		cube.visible = false;
+		showPlant(false);
 		knot.visible = true;
 		toggleWireframe();
 	}
-	else if(selectedModel === "text"){
+	else if(selectedModel === "box"){
 		knot.visible = false;
-		text1.visible = true;
-		text2.visible = true;
+		showPlant(false);
+		cube.visible = true;
+		toggleWireframe();
+	}
+	else if(selectedModel === "plant"){
+		knot.visible = false;
+		cube.visible = false;
+		showPlant(true);
 		toggleWireframe();
 	}
 })
@@ -198,11 +183,80 @@ var toggle = document.getElementById("toggle");
 toggle.addEventListener('click', toggleWireframe);
 
 function toggleWireframe(){
-	if(text1.visible){
-		text1.material.setValues({wireframe: toggle.checked});
-		text2.material.setValues({wireframe: toggle.checked});
-	}
-	else if(knot.visible){
+	if(knot.visible){
 		knot.material.setValues({wireframe: toggle.checked});
 	}
+	else if(cube.visible){
+		cube.material.setValues({wireframe: toggle.checked});
+	}
 }
+
+var light = new THREE.AmbientLight( 0xaaaaaa ); // soft white light
+scene.add( light );
+
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene.add( directionalLight );
+directionalLight.position.set(0,0,1);
+
+
+
+
+
+//TODOS===================================================
+//Add light to the scene
+// var light = new THREE.AmbientLight( 0xf0f0f0 ); // soft white light
+// scene.add( light );
+// var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+// directionalLight.position = (0,0,1);
+// scene.add( directionalLight );
+//========================================================
+//========================================================
+// var text1 = new THREE.Mesh();
+// var text2 = new THREE.Mesh();
+//
+// //Add text to the scene
+// var loader = new THREE.FontLoader();
+//
+// loader.load( 'font.json', function ( font ) {
+//
+// 	var U = new THREE.TextGeometry( 'U', {
+// 		font: font,
+// 		size: 8,
+// 		height: 20,
+// 		curveSegments: 2,
+// 	} );
+//
+// 	var F = new THREE.TextGeometry( 'F', {
+// 		font: font,
+// 		size: 8,
+// 		height: 20,
+// 		curveSegments: 2,
+// 	} );
+//
+// 	var textMaterial1 = new THREE.MeshLambertMaterial({
+// 		wireframe: false,
+// 		color: 0xff963a
+// 	});
+// 	textMaterial1.needsUpdate = true;
+// 	var textMaterial2 = new THREE.MeshLambertMaterial({
+// 		wireframe: false,
+// 		color: 0x3657f9
+// 	});
+//
+// 	text1 = new THREE.Mesh( U, textMaterial1 );
+// 	text1.visible = false;
+// 	scene.add(text1);
+// 	text2 = new THREE.Mesh( F, textMaterial2 );
+// 	text2.visible = false;
+// 	scene.add(text2);
+// 	// var pos = new THREE.Vector3(0,0,0);
+// 	// text.position = pos;
+// 	text1.position.z = -50;
+// 	text1.position.x = -4;
+// 	text1.position.y = -2;
+// 	text2.position.z = -50;
+// 	text2.position.x = 1;
+// 	text2.position.y = -2;
+// });
+//========================================================
+//========================================================
